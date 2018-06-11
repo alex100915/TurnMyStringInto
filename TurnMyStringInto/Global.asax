7d0f03dcf3215da3f2537a71f28e4af41025d1c3
mati8888 <mat.kowalski1993@gmail.com>
2018-06-11 10:11:11 +0200

    ApplicationTemplate

---
+++ TurnMyStringInto/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TurnMyStringInto.MvcApplication" Language="C#" %>
---
+++ TurnMyStringInto/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TurnMyStringInto.Startup))]
+namespace TurnMyStringInto
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
