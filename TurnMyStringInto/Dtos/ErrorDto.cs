7d702244c6f815f25398c168503e2daa01b861ce
mati8888 <mat.kowalski1993@gmail.com>
2018-06-12 10:49:30 +0200

    AddAutoMapperAndDataTransferObjects

---
+++ TurnMyStringInto/Dtos/ErrorDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TurnMyStringInto.Dtos
+{
+    public class ErrorDto
+    {
+        public int Id { get; set; }
+
+        public int Code { get; set; }
+
+        public string Message { get; set; }
+    }
+}
